24d1b61d014cbf9a73a64692914891d7069a2092
Carter Henderson <carterh062@gmail.com>
2015-12-04 20:58:37 -0500

    setup, etc

---
+++ config.production.py
@@ -0,0 +1,12 @@
+db = dict(
+    host         = 'MYSQL_HOSTNAME_GOES_HERE'
+,   user         = 'MYSQL_USERNAME_GOES_HERE'
+,   name         = 'MYSQL_DBNAME_GOES_HERE'
+,   pw           = 'MYSQL_PASSWORD_GOES_HERE'
+)
+
+redis = dict(
+    host    = 'localhost'
+,   port    = 6379
+,   db      = 0         
+)
