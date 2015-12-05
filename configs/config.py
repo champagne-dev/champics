f24eb1f37bc4e6dfff9b77a5d9e24cd05cadbac7
Jared Wright <jawerty@Jareds-MacBook-Air.local>
2015-12-05 04:50:07 -0500

    ok

--- configs/config.py
+++ configs/config.py
@@ -1,27 +1,29 @@
+import sys
+# Override default config if config_local exists (if we are local).
+db = dict(
+    host         = 'MYSQL_HOSTNAME_GOES_HERE'
+,   user         = 'MYSQL_USERNAME_GOES_HERE'
+,   name         = 'MYSQL_DBNAME_GOES_HERE'
+,   pw           = 'MYSQL_PASSWORD_GOES_HERE'
+,	port 		 = 'MYSQL_PORT_GOES_HERE'  # Has to be a string
+)
+
+
+redis = dict(
+    host    = 'DEFAULT_REDIS_HOSTNAME'
+,   port    = "DEFAULT_REDIS_PORT"
+,   db      = "DEFAULT_REDIS_DB"         
+)
+
+dirs = dict(
+	pic_dir				= "pics/"
+,	logs				= "docs/logs"
+)
+
+
+try:
+	from config_local import *
 except ImportError as e:
 	pass
 	
-	db = dict(
-	    host         = 'MYSQL_HOSTNAME_GOES_HERE'
-	,   user         = 'MYSQL_USERNAME_GOES_HERE'
-	,   name         = 'MYSQL_DBNAME_GOES_HERE'
-	,   pw           = 'MYSQL_PASSWORD_GOES_HERE'
-	,	port 		 = 'MYSQL_PORT_GOES_HERE'  # Has to be a string
-	)
-
-
-	redis = dict(
-	    host    = 'DEFAULT_REDIS_HOSTNAME'
-	,   port    = "DEFAULT_REDIS_PORT"
-	,   db      = "DEFAULT_REDIS_DB"         
-	)
-
-	dirs = dict(
-		pic_dir				= "pics/"
-	,	logs				= "docs/logs"
-	)
-
-import sys
-# Override default config if config_local exists (if we are local).
-try:
-	from config_local import *
+	
