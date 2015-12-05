3f4ae3ad98485bd4a850bc892a24cc32fa91fd24
Carter Henderson <carterh062@gmail.com>
2015-12-04 22:07:03 -0500

    initial sql done

--- docs/sql/12-4-2015_initial.sql
+++ docs/sql/12-4-2015_initial.sql
@@ -30,3 +30,13 @@ CREATE TABLE `comment` (
   `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('1', 'Funny', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('2', 'Cats', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('3', 'Dogs', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('5', 'Money', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('6', 'Democrats', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('7', 'Pictures', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('8', 'Animals', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('9', 'Games', CURRENT_TIMESTAMP, '0');
+INSERT INTO `topic` (`id`, `name`, `created_timestamp`, `post_count`) VALUES ('10', 'Republicans', CURRENT_TIMESTAMP, '0');
