d74e43578857f2e7181f9d56ea8a558de1649021
Carter Henderson <carterh062@gmail.com>
2015-12-04 22:00:25 -0500

    changed sql

--- docs/sql/12-4-2015_initial.sql
+++ docs/sql/12-4-2015_initial.sql
@@ -4,28 +4,29 @@ DROP TABLE IF EXISTS `comment`;
 
 CREATE TABLE `topic` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `post_count` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `created_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_count` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `post` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `topic_id` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `topic_id` int(11) NOT NULL,
+  `created_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(255) DEFAULT NULL,
-  `slug` varchar(255) DEFAULT NULL,
-  `score` int(11) DEFAULT NULL,
+  `slug` varchar(255) NOT NULL DEFAULT '',
+  `score` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `comment` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `text` varchar(1024) DEFAULT NULL,
-  `post_id` int(11) DEFAULT NULL,
-  `replied_id` int(11) DEFAULT NULL,
-  `score` int(11) DEFAULT NULL,
+  `text` varchar(1024) NOT NULL DEFAULT '',
+  `post_id` int(11) NOT NULL,
+  `replied_id` int(11) NOT NULL DEFAULT '-1',
+  `score` int(11) NOT NULL DEFAULT '0',
+  `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
