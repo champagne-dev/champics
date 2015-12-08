dfb2b827bb1433da631eb306637bf83029f07133
Jared Wright <jawerty@Jareds-MacBook-Air.local>
2015-12-07 21:18:09 -0500

    go

--- utils/ranking.py
+++ utils/ranking.py
@@ -4,16 +4,15 @@ from math import log
 def getRank(score, created_time):
 	if score < 0 or created_time == None:
 		return score
-	if created_time is None:
-		return score
-	else:
+	try:
 		created_timestamp = time.mktime(datetime.datetime.strptime(created_time, '%Y-%m-%d %H:%M:%S').timetuple())
 		current_timestamp = int(time.time())
 		time_diff = current_timestamp - created_timestamp
 		new_score = log(score+1)/(time_diff/60000)
 		return new_score
-
-	return score
+	except Exception as e;
+		return score
+		
 def orderTopComments(comments):
 	sorted_comments = sorted(comments, key=lambda x: getRank(int(x["score"]), x["created_timestamp"]), reverse=True)
 	return sorted_comments
