e464aeed1895fb5e5d918fb836ac6388c62e535b
Jared Wright <jawerty@Jareds-MacBook-Air.local>
2015-12-07 23:04:44 -0500

    ok

--- utils/mysql.py
+++ utils/mysql.py
@@ -24,7 +24,7 @@ def checkout_listener(dbapi_con, con_record, con_proxy):
         try:
             dbapi_con.ping(False)
         except TypeError:
-            dbapi_con.ping()
+            dbapi_con.ping(True)
     except dbapi_con.OperationalError as exc:
         if exc.args[0] in (2006, 2013, 2014, 2045, 2055):
             raise DisconnectionError()
